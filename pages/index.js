5991811c511c34496ef5ea3c0d8a3afd62286d62
tmchuynh <tinamchuynh@gmail.com>
2023-06-12 13:27:14 -0700

    update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,54 @@
-import UnstyledTable from "@/components/DynamicTable";
-import MiniDrawer from "@/components/Navbar";
+import UnstyledTable from "@/components/UnstyledTable";
+import Navbar from "@/components/Navbar";
+import { Container } from 'react-bootstrap'
+
+const data = [
+  {
+    image: "path/to/image1.jpg",
+    alt: "Card 1 image",
+    title: "Card 1 title",
+    description: "Card 1 description",
+  },
+  {
+    image: "path/to/image2.jpg",
+    alt: "Card 2 image",
+    title: "Card 2 title",
+    description: "Card 2 description",
+  },
+  {
+    image: "path/to/image2.jpg",
+    alt: "Card 2 image",
+    title: "Card 2 title",
+    description: "Card 2 description",
+  },
+  {
+    image: "path/to/image2.jpg",
+    alt: "Card 2 image",
+    title: "Card 2 title",
+    description: "Card 2 description",
+  },
+  {
+    image: "path/to/image2.jpg",
+    alt: "Card 2 image",
+    title: "Card 2 title",
+    description: "Card 2 description",
+  },
+  {
+    image: "path/to/image2.jpg",
+    alt: "Card 2 image",
+    title: "Card 2 title",
+    description: "Card 2 description",
+  },
+];
+
 
 export default function Home() {
   return (
     <div className="d-flex">
-    <MiniDrawer/>
-      <UnstyledTable/>
+      <Navbar />
+      <Container className="ms-4">
+        <UnstyledTable />
+      </Container>
     </div>
   )
 }

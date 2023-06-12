1c567b6e1cca6deb60bf3301bfa85dc296f251f3
tmchuynh <tinamchuynh@gmail.com>
2023-06-12 14:04:27 -0700

    create 1 file and delete 1 file

---
+++ pages/login/index.js
@@ -0,0 +1,25 @@
+import { Button, Container } from "react-bootstrap";
+import styles from "./Login.module.css";
+
+export default function App() {
+    return (
+        <Container fluid className={styles.loginform}>
+            <h2 className={styles.headerTitle}>Login</h2>
+            <div>
+                <div className={styles.row}>
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter your username" />
+                </div>
+                <div className={styles.row}>
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter your password" />
+                </div>
+                <div id="button" className={styles.row}>
+                    <Button>Log in</Button>
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+

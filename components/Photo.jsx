5264da9f9bb1abcec0785c5b3d19ac16ab58c3a3
tmchuynh <tinamchuynh@gmail.com>
2023-06-12 16:41:59 -0700

    update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -2,8 +2,10 @@ import Figure from 'react-bootstrap/Figure';
 
 const Photo = (props) => {
   return (
-    <Figure>
+    <Figure
+    style={{margin: 0, paddingTop: "1rem"}}>
       <Figure.Image
+        style={{ objectFit: "cover", padding: 0, margin: 0 }}
         width={props.width}
         height={props.height}
         alt={props.alt}

2c3afd099bc75c98af68fc0fa0f7bd70ff9b204e
tmchuynh <tinamchuynh@gmail.com>
2023-06-13 12:03:35 -0700

    update DataGrid.jsx and ProductService.js

--- components/DataGrid.jsx
+++ components/DataGrid.jsx
@@ -5,6 +5,9 @@ import { Column } from 'primereact/column';
 import { ProductService } from './ProductService'; import { MultiSelect } from 'primereact/multiselect';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
+import CreateIcon from '@mui/icons-material/Create';
+import Fab from '@mui/material/Fab';
+import theme from '@/styles/theme';
 
 export default function ReorderDemo() {
     const columns = [
@@ -24,22 +27,20 @@ export default function ReorderDemo() {
         ProductService.getProducts().then((data) => setProducts(data));
     }, []);
 
-    const onColumnToggle = (event) => {
-        let selectedColumns = event.value;
-        let orderedSelectedColumns = columns.filter((col) => selectedColumns.some((sCol) => sCol.field === col.field));
-
-        setVisibleColumns(orderedSelectedColumns);
-    }; 
-    const header = <MultiSelect value={visibleColumns} options={columns} optionLabel="header" onChange={onColumnToggle} className="w-full sm:w-20rem" display="chip" />;
-
     return (
         <div className="card">
-            <DataTable value={products} header={header} rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginator reorderableColumns reorderableRows onRowReorder={(e) => setProducts(e.value)} tableStyle={{ minWidth: '50rem' }}>
+            <DataTable value={products}  rows={5} rowsPerPageOptions={[5, 10, 25, 50]} paginator reorderableColumns reorderableRows onRowReorder={(e) => setProducts(e.value)} tableStyle={{ minWidth: '50rem' }}>
                 <Column rowReorder style={{ width: '3rem' }} />
                 {visibleColumns.map((col) => (
                     <Column key={col.field} field={col.field} header={col.header} />
                 ))}
             </DataTable>
+            <div className="d-flex">
+                <Fab variant="extended" type='button' href="/property/new" sx={{ margin: "1rem" }} style={{ backgroundColor: theme.palette.primary.light }}>
+                    <CreateIcon sx={{ mr: 2 }} />
+                    New Property
+                </Fab>
+            </div>
         </div>
     );
 }

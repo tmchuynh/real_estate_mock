d5eb90cd7cb1560d4558c948865390d8f4211b94
tmchuynh <tinamchuynh@gmail.com>
2023-06-13 12:18:20 -0700

    update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import EnhancedTable from "@/components/EnhancedTable";
 import Navbar from "@/components/Navbar";
 import { Container } from 'react-bootstrap'
 import CssBaseline from '@mui/material/CssBaseline';
@@ -13,7 +12,6 @@ export default function Home() {
       <Navbar />
       <Container className="d-flex flex-column">
         <Photo width="100%" height="200rem" alt="header" src="/hero.jpg"/>
-        <EnhancedTable />
         <ReorderDemo />
       </Container>
     </Box>

353282eaa440adae33aafe95c2dbfeb02b5ee742
tmchuynh <tinamchuynh@gmail.com>
2023-06-13 12:38:31 -0700

    update 1 file and create 1 file

---
+++ pages/properties.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import connection from '../database/db';
+
+export default function Users() {
+  const [property, setProperty] = useState([]);
+
+  useEffect(() => {
+    connection.query('SELECT * FROM properties', (error, results) => {
+      if (error) {
+        console.error('Error fetching users:', error);
+      } else {
+        setProperty(results);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Properties</h1>
+      <ul>
+        {property.map((item) => (
+          <li key={item.id}>{item.address}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}

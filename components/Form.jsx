ab2fba3b4d15b5d0d6b5363fb24ef3b87c5462ee
tmchuynh <tinamchuynh@gmail.com>
2023-06-13 09:55:02 -0700

    update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import Fab from '@mui/material/Fab';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import { useRouter } from 'next/router';
 
 const Jaylin = [
     {
@@ -47,104 +49,114 @@ const initialProperty = {
 }
 
 export default function Form() {
+    const router = useRouter();
     const [property, setProperty] = useState(initialProperty);
 
-    function handleSubmit() {
+    useEffect(() => {
+        const formContainer = document.getElementById('form-container');
+        formContainer.style.display = 'block';
+    }, []);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setProperty(initialProperty);
+        router.push('/property');
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Box
-                    component="form"
-                    sx={{
-                        '& .MuiTextField-root': { m: 2 },
-                    }}
-                    autoComplete="off"
-                >
-                    <TextField required id="address" label="Address" variant="standard" style={{ width: "85vw" }} />
-                    <TextField
-                        required
-                        id="price"
-                        label="Price"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
+        <div id="form-container">
+            <form onSubmit={handleSubmit}>
+                <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& .MuiTextField-root': { m: 2 },
                         }}
-                        variant="standard"
-                    />
-                    <TextField
-                        required
-                        id="bedrooms"
-                        label="Bedrooms"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        variant="standard"
-                    />
-                    <TextField
-                        required
-                        id="bathrooms"
-                        label="Bathrooms"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        variant="standard"
-                    />
-                    <TextField
-                        id="sqft"
-                        label="Sqft"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        variant="standard"
-                    />
-                    <TextField
-                        id="notes"
-                        label="Notes"
-                        multiline
-                        maxRows={4}
-                        variant="standard"
-                    />
-                    <TextField
-                        select
-                        id="jaylin"
-                        label="Jaylin"
-                        defaultValue="true"
-                        helperText="Is Jaylin allowed?"
-                        variant="standard"
+                        autoComplete="off"
                     >
-                        {Jaylin.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField
-                        select
-                        id="status"
-                        label="Status"
-                        defaultValue="new"
-                        helperText="What's the status on this one?"
-                        variant="standard"
-                    >
-                        {statues.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <br/>
-                    <Fab variant="extended" type='submit' sx={{margin: "1rem"}}>
-                        <NavigationIcon sx={{ mr: 2 }} />
-                        Create
-                    </Fab>
+                        <TextField required id="address" label="Address" variant="standard" style={{ width: "85vw" }} />
+                        <TextField
+                            required
+                            id="price"
+                            label="Price"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="standard"
+                        />
+                        <TextField
+                            required
+                            id="bedrooms"
+                            label="Bedrooms"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="standard"
+                        />
+                        <TextField
+                            required
+                            id="bathrooms"
+                            label="Bathrooms"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="standard"
+                        />
+                        <TextField
+                            id="sqft"
+                            label="Sqft"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="standard"
+                        />
+                        <TextField
+                            id="notes"
+                            label="Notes"
+                            multiline
+                            maxRows={4}
+                            variant="standard"
+                        />
+                        <TextField
+                            select
+                            id="jaylin"
+                            label="Jaylin"
+                            defaultValue="true"
+                            helperText="Is Jaylin allowed?"
+                            variant="standard"
+                        >
+                            {Jaylin.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            select
+                            id="status"
+                            label="Status"
+                            defaultValue="new"
+                            helperText="What's the status on this one?"
+                            variant="standard"
+                        >
+                            {statues.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <br />
+                        <Fab variant="extended" type='submit' sx={{ margin: "1rem" }}>
+                            <NavigationIcon sx={{ mr: 2 }} />
+                            Create
+                        </Fab>
+                    </Box>
                 </Box>
-            </Box>
-        </form>
+            </form>
+        </div>
     );
 }
